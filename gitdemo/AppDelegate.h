43f3f6123bfe94f7fe0cdb0dfb5dacf62ae1efcf
Mrtailor <2014playappsmrtailor@gmail.com>
2016-01-28 22:12:30 +0530

    second commit from local

---
+++ gitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo
+//
+//  Created by MRTailor on 28/01/16.
+//  Copyright (c) 2016 MRTailor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo
+//
+//  Created by MRTailor on 28/01/16.
+//  Copyright (c) 2016 MRTailor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
